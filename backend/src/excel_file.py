12482a2b48e55acf2789df94029de7723b60f134
Ercy Moreira Neto <ercym.neto@gmail.com>
2022-12-28 13:39:47 -0300

    feat: add get_content

--- backend/src/excel_file.py
+++ backend/src/excel_file.py
@@ -1,3 +1,4 @@
+import logging
 import hashlib
 import json
 import openpyxl
@@ -6,9 +7,11 @@ from pathlib import Path
 # BUF_SIZE is totally arbitrary, change for your app!
 BUF_SIZE = 65536
 
+
 class ExcelFile:
     name       = ""
     __has_file = ""
+    __wb_obj = None
 
     def __init__(self, name):
         self.set_name(name)
@@ -40,25 +43,43 @@ class ExcelFile:
     def __str__(self):
         return json.dumps(self.to_dict())
 
-    def print_content(self):
+    def __load_file(self):
         xlsx_file = Path('.', self.name)
-        wb_obj = openpyxl.load_workbook(xlsx_file)
-        for ws in wb_obj.worksheets:
+        self.__wb_obj = openpyxl.load_workbook(xlsx_file)
+
+    def get_content(self):
+        self.__load_file()
+        
+        file_contents = []
+        for ws in self.__wb_obj.worksheets:
+            rows_contents = []
             for row in ws.iter_rows():
-                values = []
-                key = None
-                for col in row:
-                    if col.value:
-                        if key:
-                            obj = {}
-                            obj[key.rstrip(":")] = col.value
-                            values.append(obj)
-                            key = None
-                            continue
-                        if ":" in col.value:
-                            key = col.value
-                            continue
-                        values.append(col.value)
-                print(values)
+                content = self.__get_row_content(row)
+                if len(content) > 0:
+                    rows_contents.append(content)
+            if len(rows_contents) > 0:
+                file_contents.append(dict(title=ws.title, content=rows_contents))
                 
-            print("max_columns={max_columns}, max_row={max_rows}".format(max_columns=ws.max_column, max_rows=ws.max_column))
+            logging.info("max_columns={max_columns}, max_row={max_rows}".format(max_columns=ws.max_column, max_rows=ws.max_column))
+        return file_contents
+
+    def __get_row_content(self, row):
+        content = []
+        key = None
+        for col in row:
+            if col.value:
+                if key:
+                    obj = {}
+                    obj[key.rstrip(":")] = col.value
+                    content.append(obj)
+                    key = None
+                    continue
+                if ":" in col.value:
+                    key = col.value
+                    continue
+                content.append(col.value)
+        return content
+
+    def print_content(self):
+        self.__load_file()
+        print(self.get_content())
